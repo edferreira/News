4d4e246a3d03fbfc47c009f6fa831a3f247c9584
Eduardo Ferreira <eduardo.ferreira@vortigo.com.br>
2020-10-26 22:32:33 -0300

    fix error catching not triggering onError machine state

--- src/machines/newsMachine.js
+++ src/machines/newsMachine.js
@@ -2,13 +2,11 @@ import { Machine, assign } from 'xstate';
 import axiosInstance from '../api';
 
 const fetchArticles = async (source, page) => {
-  console.log("FETCH ARTICLES")
   try {
     const res = await axiosInstance.get(`everything?sources=${source}&page=${page}`);
-    console.log('resss', res)
     return res.data;
   } catch (e) {
-    return e;
+    throw e;
   }
 };
 
@@ -37,7 +35,9 @@ const createSourcedArticlesMachine = (source) => {
               page: (context, event) => context.page + 1,
             }),
           },
-          onError: 'failure',
+          onError: {
+            target: 'failure',
+          },
         },
       },
       success: {
